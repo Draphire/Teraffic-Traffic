81c66f9617a75d6eeb14c729ba0ecf4bc503e709
WeeLiang <dmnoz@hotmail.com>
2020-12-02 21:01:48 +0800

    Initial Commit

---
+++ Teraffic Traffic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Teraffic Traffic
+//
+//  Created by WeeLiang Ng on 2/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Teraffic Traffic/Teraffic_TrafficApp.swift
@@ -0,0 +1,17 @@
+//
+//  Teraffic_TrafficApp.swift
+//  Teraffic Traffic
+//
+//  Created by WeeLiang Ng on 2/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Teraffic_TrafficApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
